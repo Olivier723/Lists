3a8d3b381a005aa465398fbb010d494c6c457fdf
Olivier <102806490+Olivier723@users.noreply.github.com>
2022-12-01 10:27:02 +0100

    Add files via upload
    Added Simple linkedlists

---
+++ linkedList.c
@@ -0,0 +1,172 @@
+#include "../include/linkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node *LinkedList_GetNode(LinkedList *linkedlist, unsigned long nodePos) // ✓
+{
+    if (nodePos >= linkedlist->listLength)
+        return NULL;
+    if (nodePos == linkedlist->listLength - 1)
+        return linkedlist->end;
+    Node *node = linkedlist->start;
+    for (unsigned long i = 0; i < nodePos; i++)
+        node = node->next;
+    return node;
+}
+
+void *LinkedList_Get(LinkedList *linkedlist, unsigned long index) // ✓
+{
+    if (index >= linkedlist->listLength)
+        return NULL;
+    Node *currentNode = LinkedList_GetNode(linkedlist, index);
+    // if (currentNode != NULL)
+    return currentNode->pointer;
+}
+
+void LinkedList_Swap(LinkedList *linkedlist, unsigned long pos1, unsigned long pos2) // ✓
+{
+    if (pos1 >= linkedlist->listLength || pos2 >= linkedlist->listLength)
+        return;
+    void *temp = LinkedList_Get(linkedlist, pos1);
+    LinkedList_ChangeNodeValue(linkedlist, LinkedList_Get(linkedlist, pos2), pos1);
+    LinkedList_ChangeNodeValue(linkedlist, temp, pos2);
+}
+
+void LinkedList_ChangeNodeValue(LinkedList *linkedlist, void *item, unsigned long index) // ✓
+{
+    if(index >= linkedlist->listLength) return;
+    Node *tempNode = LinkedList_GetNode(linkedlist, index);
+    tempNode->pointer = item;
+}
+
+void LinkedList_Print(LinkedList *linkedlist, void (*printFunc)(void *)) // ✓
+{
+    if (LinkedList_isEmpty(linkedlist))
+        return;
+    Node *currentNode = linkedlist->start;
+    for (unsigned long i = 0; i < linkedlist->listLength - 1; i++)
+    {
+        printFunc(currentNode->pointer);
+        currentNode = currentNode->next;
+        printf("->");
+    }
+    printFunc(currentNode->pointer);
+    printf("\n");
+}
+
+int LinkedList_isEmpty(LinkedList *linkedlist) // ✓
+{
+    return linkedlist->start == NULL;
+}
+
+void LinkedList_FreeRecNode(void(*freeElemFunc)(void *), Node* nodeToFree){
+    if(nodeToFree == NULL){
+        return;
+    }
+    LinkedList_FreeRecNode(freeElemFunc, nodeToFree->next);
+    freeElemFunc(nodeToFree->pointer);
+    free(nodeToFree);
+
+}
+
+void LinkedList_FreeRec(LinkedList* linkedlist, void(*freeElemFunc)(void*)){
+    LinkedList_FreeRecNode(freeElemFunc,linkedlist->start);
+    free(linkedlist);
+}
+
+void LinkedList_Free(LinkedList *linkedlist, void(*freeElemFunc)(void*)) // ✓(Valgrind)
+{
+    Node **tempList = malloc(sizeof(Node *) * linkedlist->listLength);
+    Node *currentNode = linkedlist->start;
+    for (unsigned long i = 0; i < linkedlist->listLength; i++)
+    {
+        tempList[i] = currentNode;
+        currentNode = currentNode->next;
+    }
+    for (unsigned long j = 0; j < linkedlist->listLength; j++)
+    {
+        freeElemFunc(tempList[j]->pointer);
+        free(tempList[j]);
+    }
+    free(linkedlist);
+    free(tempList);
+}
+
+void LinkedList_Remove(LinkedList *linkedlist, unsigned long index) // ✓
+{
+    if (index >= linkedlist->listLength)
+        return;
+    Node *toFree;
+    if (index == 0)
+    {
+        toFree = linkedlist->start;
+        linkedlist->start = toFree->next;
+        free(toFree);
+        linkedlist->listLength--;
+        return;
+    }
+    Node *currentNode = LinkedList_GetNode(linkedlist, index - 1);
+    toFree = currentNode->next;
+    currentNode->next = currentNode->next->next;
+    free(toFree);
+    linkedlist->listLength--;
+}
+
+void LinkedList_Insert(LinkedList *linkedlist, void *item, unsigned long index) // ✓
+{
+    if (index > linkedlist->listLength)
+        return;
+    if (linkedlist->listLength - 1 == index)
+    {
+        LinkedList_Append(linkedlist, item);
+        return;
+    }
+    Node *newNode = malloc(sizeof(Node));
+    newNode->pointer = item;
+    linkedlist->listLength++;
+    Node *tempNode;
+    if (index == 0)
+    {
+        tempNode = linkedlist->start;
+        linkedlist->start = newNode;
+        newNode->next = tempNode;
+        return;
+    }
+    Node *currentNode = LinkedList_GetNode(linkedlist, index - 1);
+    tempNode = currentNode->next;
+    currentNode->next = newNode;
+    newNode->next = tempNode;
+}
+
+void LinkedList_Append(LinkedList *linkedlist, void *item) // ✓
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->pointer = item;
+    newNode->next = NULL;
+    linkedlist->listLength++;
+    if (LinkedList_isEmpty(linkedlist))
+    {
+        linkedlist->start = newNode;
+        linkedlist->end = newNode;
+        return;
+    }
+    linkedlist->end->next = newNode;
+    linkedlist->end = newNode;
+}
+
+void LinkedList_ToDynamic(LinkedList *linkedList, void **dynamicList)
+{
+    for (unsigned long i = 0; i < linkedList->listLength; i++)
+    {
+        dynamicList[i] = LinkedList_Get(linkedList, i);
+    }
+}
+
+LinkedList *createLinkedList() // ✓
+{
+    LinkedList *linkedlist = malloc(sizeof(LinkedList));
+    linkedlist->start = NULL;
+    linkedlist->end = NULL;
+    linkedlist->listLength = 0;
+    return linkedlist;
+}
---
+++ linkedList.h
@@ -0,0 +1,49 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node Node;
+struct Node
+{
+    void *pointer;
+    Node *next;
+};
+
+/*Encapsulates the linked list in a single struct with the length to make it more convenient*/
+typedef struct LinkedList LinkedList;
+struct LinkedList
+{
+    Node *start;
+    Node *end;
+    unsigned long listLength;
+};
+
+//Changes the value stored in the node at index position
+void LinkedList_ChangeNodeValue(LinkedList* linkedlist, void* Elem, unsigned long index);
+
+/*
+@param pos1 The position of the node 1
+@param pos2 The position of the node 2
+Swaps the values of both nodes
+*/
+void LinkedList_Swap(LinkedList *list, unsigned long pos1, unsigned long pos2);
+
+void LinkedList_Print(LinkedList* linkedlist, void (*printFunc)(void*));
+
+int LinkedList_isEmpty(LinkedList* linkedlist);
+
+void LinkedList_Free(LinkedList* linkedlist, void(*freeElemFunc)(void*));
+
+void LinkedList_FreeRec(LinkedList* linkedlist, void(*freeElemFunc)(void *));
+
+void LinkedList_Remove(LinkedList* linkedlist, unsigned long index);
+
+void LinkedList_Insert(LinkedList* linkedlist, void* Elem,unsigned long index);
+
+void LinkedList_Append(LinkedList* linkedlist, void* Elem);
+
+//Gets the value stored in the node at index
+void* LinkedList_Get(LinkedList* linkedlist, unsigned long index);
+
+LinkedList* createLinkedList();
+
+#endif
